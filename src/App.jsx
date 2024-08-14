b141485dac7867fb4e1bce390bf2beb2e7b9cdb4
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-14 11:30:35 -0300

    Criando aplicação da aula de front

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Componente1 from "./components/Componente1"
+
+
+function App() {
+  
+  const nome = 'Thiago Oliveira'
+
+  return (
+    <>
+      <h1>Primeira Aula</h1>
+      <br />
+      <p>nossa primeira aula - {nome} </p>
+      <Componente1/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Componente1.jsx
@@ -0,0 +1,17 @@
+import logoreact from "../assets/react.svg";
+
+
+
+
+function Componente1(){
+
+    return(
+        <>
+        <h2>Componente 1</h2>
+        <img src={logoreact} alt="logo do react" />
+        </>
+        
+    )
+}
+
+export default Componente1
